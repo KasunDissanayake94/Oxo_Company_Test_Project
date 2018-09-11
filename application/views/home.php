60bb437e2e6805173dea51107cd40c11d64a5aa3
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2018-09-11 15:49:20 +0530

    Add styles for the Login

--- application/views/home.php
+++ application/views/home.php
@@ -124,6 +124,11 @@
                 <i class="fas fa-fw fa-table"></i>
                 <span>Tables</span></a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="tables.html">
+                <i class="fas fa-id-card"></i>
+                <span>User Registration</span></a>
+        </li>
     </ul>
 
     <div id="content-wrapper">
