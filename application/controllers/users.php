7ebcd850badcfdd12a4c4ee62e36326738b07e59
pramodaya <pramodayajayalath@gmail.com>
2018-09-11 12:30:57 +0530

    edited the login

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -12,55 +12,23 @@ class Users extends CI_Controller {
 		$this->load->view('register_form');
 	}
 
-
 	public function login(){
 
-		
-		
-		
-	
-			$email = $_POST['email'];
-			$password = $_POST['password'];
-
-
-		$this->form_validation->set_rules('email','Email','required');
-		$this->form_validation->set_rules('password','Password','required');
-
-		if($this->form_validation->run()== FALSE){
-
-			// redirect('users/index');
-			echo 'invalid';
-		}
-		else{
 			$email = $this->input->post('email');
 			$password = $this->input->post('password');
-
 
 			$this->load->model('user_model');
 			$user_id = $this->user_model->login_user($email,$password);
 
-			if($user_id){
-
-			
+			if($user_id){		
 				$user_data = array(
 						'user_id' => $user_id,
 						'email' => $email,
 						'logged_in' => true
-					);
+				);
 
 					$this->session->set_userdata($user_data);
 					$this->session->set_flashdata('login_success','you are now logged in');
-
-
-
-				// if logged in, show the view
-
-				// $this->load->view('home',$user_data);
-				echo 'all ok';
-
-
-					// if logged in, show the view 
-					
 					$this->load->view('home',$user_data);
 			}else{
 
@@ -69,12 +37,9 @@ class Users extends CI_Controller {
 				redirect('Welcome/login');
 			}
 		
-		
+	}
 
-				// redirect('users/index');
-				echo 'no perwon in db';
-			}
-		}
+			
 	
 	//User Registration
 
